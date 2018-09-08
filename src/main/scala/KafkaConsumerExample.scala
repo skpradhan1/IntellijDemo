ba80a946b96ef6d6fe91b2d280f03fc256ed6772
skpradhan1 <pradhan.susant@gmail.com>
2018-09-08 11:49:23 -0400

    Initial commit

---
+++ src/main/scala/KafkaConsumerExample.scala
@@ -0,0 +1,13 @@
+import java.util.{Properties,Collections}
+import org.apache.kafka.clients.consumer.{ConsumerConfig,KafkaConsumer}
+
+
+
+object KafkaConsumerExample {
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+}
---
+++ src/main/scala/KafkaProducerExample.scala
@@ -0,0 +1,29 @@
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import com.typesafe.config.ConfigFactory
+
+object KafkaProducerExample {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = ConfigFactory.load()
+    val envProp = conf.getConfig(args(0))
+
+    val prop = new Properties()
+    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, envProp.getString("bootstrap.server"))
+
+    prop.put(ProducerConfig.CLIENT_ID_CONFIG,"KafkaProducerExample")
+    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer")
+    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringSerializer")
+
+    val producer = new KafkaProducer[String,String](prop)
+    val data = new ProducerRecord[String,String]("kafka-testing","key1", "value1")
+    producer.send(data)
+
+  }
+
+}
